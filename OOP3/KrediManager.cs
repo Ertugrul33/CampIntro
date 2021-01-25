98178078bd3f4d15d0acb6c56a36eae4c8d83aed
Ertuğrul EŞOL <ertugrul.esol@gmail.com>
2021-01-25 18:28:01 +0300

    Güncellendi.

--- OOP3/KrediManager.cs
+++ OOP3/KrediManager.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace OOP3
 {
     //Interface'leri birbirinin alternatifi olan ama kod içerikleri farklı olan durumlar için kullanılır.
-    interface IKrediManager //Tek başına bir anlam ifade etmez, sadece ebeveyn görevi gördüğü için interface dendi. Okunurluğu arttırmak için başına "I" konulur. Şablon demektir. Operasyon sınıflarında kullanılır. Aynı zamanda referans tutucu görevi görür.
+    interface IKrediManager //Tek başına bir anlam ifade etmez, sadece ebeveyn görevi gördüğü için interface dendi. Okunurluğu arttırmak için başına "I" konulur. Şablon demektir. Genelde operasyon sınıflarında kullanılır. Aynı zamanda referans tutucu görevi görür.
     {
         void Hesapla();
         void BiseyYap();
