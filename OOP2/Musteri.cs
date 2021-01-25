afe1316ca7f83828e96a346d91a244e647bcf1ae
Ertuğrul EŞOL <ertugrul.esol@gmail.com>
2021-01-25 18:26:43 +0300

    Güncellendi.

--- OOP2/Musteri.cs
+++ OOP2/Musteri.cs
@@ -6,7 +6,7 @@ namespace OOP2
 {
     //Not: Eğer bir nesnede bir değeri kullanmak zorunda gibi görünmüyorsa soyutlama hatası vardır demektir. (İlk dakikalar)
     //Halledildi. (Yaklaşık yarım saat sonra)
-    class Musteri //Base Class
+    class Musteri //Base Class (Aynı zamanda özellik sınıfı)
     {
         public int Id { get; set; }
         public string MusteriNo { get; set; }
